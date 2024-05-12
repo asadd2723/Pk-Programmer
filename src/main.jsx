40134c2c6f13c2ddf8201a815534af2edf6bdc64
asadd2723 <asadu3040@gmail.com>
2024-05-12 23:57:19 +0500

    change in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -23,10 +23,6 @@ const router = createBrowserRouter([
       {
         path:"/about",
         element:<About />
-      },
-      {
-        path:"/github",
-        element:<About />
       }
     ]
   }
