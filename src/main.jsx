e6deb645b12bf293035c0633a404139613038b42
asadd2723 <asadu3040@gmail.com>
2024-05-12 22:46:52 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Contact from './components/Contact/Contact.jsx'
+import About from './components/About/About.jsx'
+import Home from './components/Home/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App />,
+    children:[
+      {
+        path:"",
+        element: <Home/>
+      },
+      {
+        path:"/contact",
+        element:<Contact />
+      },
+      {
+        path:"/about",
+        element:<About />
+      },
+      {
+        path:"/github",
+        element:<About />
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
